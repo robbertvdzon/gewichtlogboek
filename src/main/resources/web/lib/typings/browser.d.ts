426a0aa14582d964bb370c38eb927cd0e5e46327
Robbert <robbert@vdzon.com>
2016-02-27 17:56:56 +0100

    update

---
+++ src/main/resources/web/lib/typings/browser.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="browser\ambient\angular-animate\angular-animate.d.ts" />
+/// <reference path="browser\ambient\angular\angular.d.ts" />
+/// <reference path="browser\ambient\require\require.d.ts" />
---
+++ src/main/resources/web/lib/typings/main.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="main\ambient\angular-animate\angular-animate.d.ts" />
+/// <reference path="main\ambient\angular\angular.d.ts" />
+/// <reference path="main\ambient\require\require.d.ts" />
